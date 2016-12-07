2a80a6e7147227af89e83cdbfaaf68b45a1e1f07
Jacque Schrag <jnschrag@gmail.com>
2016-12-07 14:12:49 -0500

    Only submit menu items if we're on the hero menu

--- wp-content/plugins/hero-menu/hero-menu.php
+++ wp-content/plugins/hero-menu/hero-menu.php
@@ -166,28 +166,34 @@ class HeroMenu {
 	 * @return      void
 	*/
 	function js_hm_update_custom_nav_fields( $menu_id, $menu_item_db_id, $args ) {
+
+		$locations = get_nav_menu_locations();
+		$sliderMenuID = $locations['hero-menu'];
+
+		if($menu_id == $sliderMenuID) {
 	
-	    // Check if element is properly sent
-	    if ( is_array( $_REQUEST['menu-item-featured-image']) ) {
-	        $featured_image_value = $_REQUEST['menu-item-featured-image'][$menu_item_db_id];
-	        update_post_meta( $menu_item_db_id, '_menu_item_featured_image', $featured_image_value );
-	    }
+		    // Check if element is properly sent
+		    if ( is_array( $_REQUEST['menu-item-featured-image']) ) {
+		        $featured_image_value = $_REQUEST['menu-item-featured-image'][$menu_item_db_id];
+		        update_post_meta( $menu_item_db_id, '_menu_item_featured_image', $featured_image_value );
+		    }
 
-	    if ( is_array( $_REQUEST['menu-item-cta']) ) {
-	        $cta_value = $_REQUEST['menu-item-cta'][$menu_item_db_id];
-	        update_post_meta( $menu_item_db_id, '_menu_item_cta', $cta_value );
-	    }
+		    if ( is_array( $_REQUEST['menu-item-cta']) ) {
+		        $cta_value = $_REQUEST['menu-item-cta'][$menu_item_db_id];
+		        update_post_meta( $menu_item_db_id, '_menu_item_cta', $cta_value );
+		    }
 
-	    if ( is_array( $_REQUEST['menu-item-bgpos-x']) ) {
-	        $bgpos_x_value = $_REQUEST['menu-item-bgpos-x'][$menu_item_db_id];
-	        update_post_meta( $menu_item_db_id, '_menu_item_bgpos_x', $bgpos_x_value );
-	    }
+		    if ( is_array( $_REQUEST['menu-item-bgpos-x']) ) {
+		        $bgpos_x_value = $_REQUEST['menu-item-bgpos-x'][$menu_item_db_id];
+		        update_post_meta( $menu_item_db_id, '_menu_item_bgpos_x', $bgpos_x_value );
+		    }
 
-	    if ( is_array( $_REQUEST['menu-item-bgpos-y']) ) {
-	        $bgpos_y_value = $_REQUEST['menu-item-bgpos-y'][$menu_item_db_id];
-	        update_post_meta( $menu_item_db_id, '_menu_item_bgpos_y', $bgpos_y_value );
-	    }
-	    
+		    if ( is_array( $_REQUEST['menu-item-bgpos-y']) ) {
+		        $bgpos_y_value = $_REQUEST['menu-item-bgpos-y'][$menu_item_db_id];
+		        update_post_meta( $menu_item_db_id, '_menu_item_bgpos_y', $bgpos_y_value );
+		    }
+		}
+		    
 	}
 	
 	/**
